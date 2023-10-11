a2e511b9ad8d8690b06dcf3cd3486508d6c36d0a
evaluacion <pipicanodayana2005@gmail.com>
2023-10-11 14:00:40 -0500

    footer

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -14,8 +14,10 @@
     
     @section('head')
     <title>Contacto - Perfumería</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+ 
+    <link rel="shortcut icon" href="IMG/LOGO.png" type="image/x-icon">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Libre+Baskerville&family=Quicksand:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/pago.css')}}">
     @endsection()
     
