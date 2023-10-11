a738e999d7f519ee8bae0470886a356afe4e329b
quizvale <valentinarengifonarvaez1111@gmail.com>
2023-10-11 13:33:26 -0500

    init

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.formulario')
 
 @section('content')
+@include('layouts.head')
     <div class="container">
         
             <h1 class="mt-4">Productos<i class="fas fa-box-open"></i></h1>
