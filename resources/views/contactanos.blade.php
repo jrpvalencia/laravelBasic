b5719202d0585902b59e375355d58393acf1b89a
evaluacion <pipicanodayana2005@gmail.com>
2023-10-11 13:28:49 -0500

    contacto

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -6,9 +6,20 @@
     <title>Contacto - Perfumería</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('css/pago.css')}}"> --}}
+        
+    @extends('layouts.plantilla')
+
+    @section('title','Conocenos')
+    
+    @section('head')
+    <title>Contacto - Perfumería</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="{{asset('css/pago.css')}}">
-        
-        
+    @endsection()
+    
+    @section('content')
 </head>
 <body>
     <div class="container">
@@ -45,3 +56,4 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
 </body>
 </html>
+@endsection()
