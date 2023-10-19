482048bd02e62b32784a1ac9dd747c4d3228d15e
laravelBasic <pipicanodayana2005@gmail.com>
2023-10-19 16:46:32 -0500

    contacto

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -11,36 +11,98 @@
 @endsection()
 
 @section('content')
-    <div class="container">
-        <div class="contacto-heading">
-            <h1>Contacto</h1>
-            <p>¡Estamos aquí para ayudarte! Contáctanos para cualquier consulta o comentario.</p>
-        </div>
-        <div class="contacto-formulario">
-            <form action="#" method="post">
-                <div class="mb-3">
-                    <label for="nombre" class="formulario-label">Nombre:</label>
-                    <input type="text" class="form-control formulario-control" id="nombre" name="nombre" required>
-                </div>
-                <div class="mb-3">
-                    <label for="correo" class="formulario-label">Correo Electrónico:</label>
-                    <input type="email" class="form-control formulario-control" id="correo" name="correo" required>
-                </div>
-                <div class="mb-3">
-                    <label for="mensaje" class="formulario-label">Mensaje:</label>
-                    <textarea class="form-control formulario-control" id="mensaje" name="mensaje" rows="5" required></textarea>
-                </div>
-                <div class="text-center">
-                    <button type="submit" class="btn btn-enviar"><i class="fas fa-paper-plane icono"></i> Enviar Mensaje</button>
-                </div>
-            </form>
-        </div>
-        <div class="text-center mt-3">
-            <a href="javascript:history.back()" class="btn btn-secondary"><i class="fas fa-arrow-left"></i> Regresar</a>
-        </div>
-    </div>
+<!DOCTYPE html>
 
+<div class="html">
+
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="{{ asset('css/contact.css') }}" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+
+<body>
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+
+    <div class="body">
+        
+
+
+            <div class="formu">
+                <div class="contact-info">
+                    <h3 class="title">Let's get in touch</h3>
+                    <p class="text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+                        dolorum adipisci recusandae praesentium dicta!
+                    </p>
+
+                    <div class="info">
+                        <div class="information">
+                            <img src="img/localizacion.png" class="icon" alt="" />
+                            <p>92 Cherry Drive Uniondale, NY 11553</p>
+                        </div>
+                        <div class="information">
+                            <img src="img/correo.png" class="icon" alt="" />
+                            <p>lorem@ipsum.com</p>
+                        </div>
+                        <div class="information">
+                            <img src="img/telefono.png" class="icon" alt="" />
+                            <p>123-456-789</p>
+                        </div>
+                    </div>
+
+                    <div class="social-media">
+                        <p>Connect with us :</p>
+                        <div class="social-icons">
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="contact-form">
+                    <span class="circle one"></span>
+                    <span class="circle two"></span>
+
+                    <form action="index.html" autocomplete="off">
+                        <h3 class="title">Contact us</h3>
+                        <div class="input-container">
+                            <input type="text" name="name" class="input" placeholder="Usuario" />
+
+                            <span>Username</span>
+                        </div>
+                        <div class="input-container">
+                            <input type="email" name="email" class="input" placeholder="Email" />
+
+                            <span>Email</span>
+                        </div>
+                        <div class="input-container">
+                            <input type="tel" name="phone" class="input" placeholder="Telefono" />
+
+                            <span>Phone</span>
+                        </div>
+                        <div class="input-container textarea">
+                            <textarea name="message" class="input" placeholder="Mensage"></textarea>
+
+                            <span>Message</span>
+                        </div>
+                        <input type="submit" value="Send" class="btn" />
+                    </form>
+                </div>
+            </div>
+       
+
+        <script src="{{ asset('css/contact.js') }}"></script>
+    </div>
+</body>
+</div>
+
 @endsection()
